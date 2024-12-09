a24bf360f6350c70da4d808a3b44c753d224d9ff
吉田竜一郎 <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-12-09 20:23:14 +0900

    [fix] pathの修正

--- build.bat
+++ build.bat
@@ -2,18 +2,25 @@
 chcp 65001 > nul
 
 @REM 変数の定義
-set BUILD_TOOL_PATH="D:\UE\UE_5.5\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe"
-set PROJECT_PATH="C:\UE\TeamD"
-set VS_MS_BUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-set BUILD_BATCH_PATH="D:\UE\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat"
-set EXPORT_PATH="G:\その他のパソコン\マイ コンピュータ\Artifacts\Team2024\TeamD"
+set BUILD_TOOL_PATH=D:\UE\UE_5.5\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe
+set PROJECT_PATH=C:\UE\TeamD
+set VS_MS_BUILD_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe
+set BUILD_BATCH_PATH=D:\UE\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat
+set EXPORT_PATH=G:\その他のパソコン\マイ コンピュータ\Artifacts\Team2024\TeamD
 set GAS_URI="GASのURI"
 
 call :setup
 @REM UEプロジェクトをビルド、コンパイル、パッケージングするコマンド
 "%BUILD_BATCH_PATH%" BuildCookRun -rocket -compile -compileeditor -installed -nop4 -project="%PROJECT_PATH%\TeamD.uproject" -cook -stage -archive -archivedirectory="%PROJECT_PATH%\temp\Development\x64" -package -clientconfig=Shipping -ue5exe=UnrealEditor-Cmd.exe -clean -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -utf8output
 
-move /Y "%PROJECT_PATH%\TeamA.zip" %EXPORT_PATH%
+@REM ビルドファイルを圧縮してGoogleDriveへ配置
+powershell -NoProfile -ExecutionPolicy Unrestricted Compress-Archive -Path "%PROJECT_PATH%\Build" -DestinationPath "TeamD.zip" -Force
+
+if not %errorlevel% == 0 (
+	exit /b 1
+)
+
+move /Y "%PROJECT_PATH%\TeamD.zip" %EXPORT_PATH%
 if not %errorlevel% == 0 (
 	pause
 )
